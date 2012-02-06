d47226b528653d40b59a1b9b201178f1466792ff
karmajunkie <keith.gaddis@gmail.com>
2012-02-06 15:07:39 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traveljournal::Application.config.secret_token = '66f13d79620bcd4c9c7f8f974b60e2d5f3de0d178140ac13f6db2ff8f5f9a20c353408bd06e8694e20814982adb73e0ae2ff73d93792670d943faf69450f0822'
